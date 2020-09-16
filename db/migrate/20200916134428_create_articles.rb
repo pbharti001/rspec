e92d7c1a241e33ce5ae01cd5410c84284c7d3389
P-one-1 <bpawan96@gmail.com>
2020-09-16 19:22:44 +0530

    first commit g welcome, scaffold user & article

---
+++ db/migrate/20200916134428_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :text
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
