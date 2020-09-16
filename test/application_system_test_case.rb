17195dc14216069c12634d2e88189f299b92ebd9
P-one-1 <bpawan96@gmail.com>
2020-09-16 19:46:31 +0530

    Added system test.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
+# require "capybara/poltergeist"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :firefox, screen_size: [1400, 1400] #If u r using headless_firefox or headless_chrome the screenshot is not going to pop-up.
+  # driven_by :poltergeist
 end
