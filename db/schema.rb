5d7d773ce00a41d680a7d67957c38fd1650c6624
P-one-1 <bpawan96@gmail.com>
2021-01-15 16:21:25 +0530

    added circleci and gemset.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_16_134428) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "text"
     t.text "body"
